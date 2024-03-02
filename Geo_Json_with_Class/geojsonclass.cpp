c84048fa6584354d1c2dbe66c42489ae6df4d3dc
SoheylSp <144216711+SoheylSp@users.noreply.github.com>
2024-03-02 13:53:04 +0330

    Add files via upload

---
+++ Geo_Json_with_Class/geojsonclass.cpp
@@ -0,0 +1,6 @@
+#include "geojsonclass.h"
+
+GeojsonClass::GeojsonClass()
+{
+
+}
---
+++ Geo_Json_with_Class/geojsonclass.h
@@ -0,0 +1,11 @@
+#ifndef GEOJSONCLASS_H
+#define GEOJSONCLASS_H
+
+
+class GeojsonClass
+{
+public:
+    GeojsonClass();
+};
+
+#endif // GEOJSONCLASS_H
